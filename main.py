b79c7e1e1db5b1a5b57e99a136bc4ba15d77b0d4
Aryan Goel <aryangoel600@gmail.com>
2026-06-24 04:43:25 +0530

    Add files via upload
    In this, is the code which is being directed to the codecrafters which examines wheter i am successful in completing the challenge or not

---
+++ main.py
@@ -0,0 +1,51 @@
+import sys
+import os
+
+def main():
+    builtins = ['echo', 'type', 'exit']
+
+    while True:
+        sys.stdout.write("$ ")
+
+        command = input()
+
+        if command == "exit":
+            break
+
+        elif command.startswith("echo"):
+            print(command[5:])
+
+        elif command.startswith("type"):
+            disect = command.split()
+            cmd = disect[1]
+
+            # Check builtins first
+            if cmd in builtins:
+                print(f"{cmd} is a shell builtin")
+
+            else:
+                found = False
+
+                # Read PATH
+                path_dirs = os.environ.get("PATH", "").split(os.pathsep)
+
+                # Search every directory
+                for directory in path_dirs:
+                    full_path = os.path.join(directory, cmd)
+
+                    if (
+                        os.path.isfile(full_path)
+                        and os.access(full_path, os.X_OK)
+                    ):
+                        print(f"{cmd} is {full_path}")
+                        found = True
+                        break
+
+                if not found:
+                    print(f"{cmd}: not found")
+
+        else:
+            print(f"{command}: command not found")
+
+if __name__ == "__main__":
+    main()
